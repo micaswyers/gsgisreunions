17f580948ca8ab82eb23585e58f6dd681aa73e22
Mica Swyers <micaswyers@gmail.com>
2014-10-07 20:56:31 -0700

    Adding pagination that actually works

--- app.py
+++ app.py
@@ -4,35 +4,30 @@ import os
 import requests
 
 
-NEXT_URL = None
 app = Flask(__name__)
+INITIAL_REQUEST_URL="https://api.instagram.com/v1/tags/gsgisreunions/media/recent?client_id=b1da9d7eeb1a4f638234ff7d846b008a"
+NEXT_URL = None
+
+def call_api(request=INITIAL_REQUEST_URL):
+    r = requests.get(request)
+    global NEXT_URL
+    NEXT_URL = r.json()['pagination']['next_url']
+    return map(lambda x: x['images']['standard_resolution']['url'], r.json()['data'])
 
 @app.route("/", )
 def index():
-    r = requests.get("https://api.instagram.com/v1/tags/frenchbulldog/media/recent?client_id=b1da9d7eeb1a4f638234ff7d846b008a")
-
-    urls_list = map(lambda x: x['images']['standard_resolution']['url'], r.json()['data'])
-    global NEXT_URL
-    print "THIS IS THE NEXT URL"
-    NEXT_URL = r.json()['pagination']['next_url']
-    print NEXT_URL
-    return render_template('index.html', urls_list = urls_list)
+    urls_list =  call_api()
+    return render_template('index.html', urls_list=urls_list)
 
 @app.route("/next", )
 def get_more_images():
-    r = requests.get(NEXT_URL)
-    print "NOW THIS IS NEXT URL"
-    print r.json()['pagination']['next_url']
     global NEXT_URL
-    NEXT_URL = r.json()['pagination']['next_url']
-    urls_list = map(lambda x: x['images']['standard_resolution']['url'], r.json()['data'])
-    return render_template('index.html', urls_list = urls_list)
-
-
+    urls_list = call_api(NEXT_URL)
+    return render_template('index.html', urls_list=urls_list)
 
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
