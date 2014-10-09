6de79a7a327b8c42f2785d69e519dd487348eb34
Mica Swyers <micaswyers@gmail.com>
2014-10-08 21:45:03 -0700

    Adding button to load more images

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ import requests
 
 app = Flask(__name__)
 INITIAL_REQUEST_URL="https://api.instagram.com/v1/tags/frenchbulldog/media/recent?client_id=b1da9d7eeb1a4f638234ff7d846b008a"
-
+NEXT_URL = None
 def call_api(request=INITIAL_REQUEST_URL):
+    global NEXT_URL
     r = requests.get(request)
-
+    NEXT_URL = r.json()['pagination']['next_url']
     urls_list = map(lambda x: x['images']['standard_resolution']['url'], r.json()['data'])
+
     return urls_list
 
 @app.route("/", )
@@ -18,9 +20,12 @@ def index():
     urls_list =  call_api()
     return render_template('index.html', urls_list=urls_list)
 
-@app.route("/next", )
+@app.route("/next", methods=["GET"])
 def get_more_images():
-    pass
+    urls_list = call_api(NEXT_URL)
+    return json.dumps(
+        {"urls": urls_list}
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- static/myjs.js
+++ static/myjs.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-    console.log('ready!');
-    $(".hidden").hide();
-    });
+function makeNewImgTags(urls_list) {
+    for (var i =0; i < urls_list.length; i++) {
+        $(".main_container").append("<img src='" + urls_list[i] + "'>")
+    }
+}
+
+$(document.body).append('<button type="button" id="more">More</button>');
 
 $(window).scroll(function() {
 if ($(this).scrollTop() > 1){
@@ -12,4 +15,19 @@ if ($(this).scrollTop() > 1){
           $('header').removeClass("sticky");
         $(".hidden").hide();
             }
+});
+
+$("#more").click (function(e) {
+    e.preventDefault();
+    console.log("User clicked");
+
+    $.ajax({
+        url: "/next",
+    }).done(function(response) {
+        var returned_dict = $.parseJSON(response);
+        var new_urls_list = returned_dict['urls'];
+        console.log("Data is back");
+        makeNewImgTags(new_urls_list);
+        console.log("Did you ruN?");
+    })
 });
